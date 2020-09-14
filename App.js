382c13307f56a697ef66443d220a8d6b14fc1cd6
vimal Raveendran <vimalraveendra@gmail.com>
2020-09-14 15:38:08 +0200

    Updated App component to render the users list, also delete the entire list

--- App.js
+++ App.js
@@ -1,19 +1,61 @@
 import React from 'react';
 
-import {SafeAreaView, Text, StyleSheet} from 'react-native';
-import {ButtonGroup} from 'react-native-elements';
+import {SafeAreaView, Text, StyleSheet, FlatList} from 'react-native';
+import {ButtonGroup, ListItem, Avatar, CheckBox} from 'react-native-elements';
+import Icon from 'react-native-vector-icons/Feather';
+import FontIcon from 'react-native-vector-icons/FontAwesome';
+
+Icon.loadFont();
+FontIcon.loadFont();
 
 class App extends React.Component {
   state = {
     selectedIndex: 0,
+    listItems: [
+      {
+        id: 1,
+        name: 'Amy Farha',
+        avatar_url:
+          'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+        subtitle: 'Vice President',
+      },
+      {
+        id: 2,
+        name: 'Chris Jackson',
+        avatar_url:
+          'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+        subtitle: 'Vice Chairman',
+      },
+    ],
+    deletedList: [],
   };
   updateIndex = (selectedIndex) => {
-    this.setState({selectedIndex});
+    if (selectedIndex === 2) {
+      this.setState({selectedIndex, listItems: []});
+    } else if (selectedIndex === 1) {
+      this.setState({selectedIndex});
+    } else {
+      this.setState({selectedIndex});
+    }
   };
 
-  renderItem = () => {};
+  renderItem = ({item}) => {
+    return (
+      <ListItem bottomDivider>
+        <CheckBox />
+        <Avatar rounded source={{uri: item.avatar_url}} />
+        <ListItem.Content>
+          <ListItem.Title>{item.name}</ListItem.Title>
+          <ListItem.Subtitle>{item.subtitle}</ListItem.Subtitle>
+        </ListItem.Content>
+        <FontIcon name="trash-o" color="red" size={25} />
+        <Icon name="chevron-right" color="#eee" size={30} />
+      </ListItem>
+    );
+  };
+
   render() {
-    const buttons = ['Hello', 'World', 'Buttons'];
+    const buttons = ['Delete Selected', 'Select All', 'DeleteAll'];
     const {selectedIndex} = this.state;
     return (
       <SafeAreaView style={styles.container}>
@@ -25,8 +67,12 @@ class App extends React.Component {
           containerStyle={{
             marginTop: 10,
             borderColor: 'green',
-            textStyle: 'blue',
           }}
+        />
+        <FlatList
+          data={this.state.listItems}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={this.renderItem}
         />
       </SafeAreaView>
     );
@@ -36,11 +82,12 @@ class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
   },
   title: {
     fontSize: 23,
     fontWeight: '700',
+    alignSelf: 'center',
+    marginTop: 10,
   },
 });
 
