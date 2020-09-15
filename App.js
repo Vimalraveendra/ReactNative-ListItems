4aa977cd3b04aac7d473f5e192f3058a8269c1a2
vimal Raveendran <vimalraveendra@gmail.com>
2020-09-15 11:09:52 +0200

    Created Components folder and add DataList also updated App component to select all the users in the list

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
-import {SafeAreaView, Text, StyleSheet, FlatList} from 'react-native';
+import {
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
 import {ButtonGroup, ListItem, Avatar, CheckBox} from 'react-native-elements';
 import Icon from 'react-native-vector-icons/Feather';
 import FontIcon from 'react-native-vector-icons/FontAwesome';
@@ -8,54 +14,70 @@ import FontIcon from 'react-native-vector-icons/FontAwesome';
 Icon.loadFont();
 FontIcon.loadFont();
 
+import DatList from './Components/DataList/DataList';
+
 class App extends React.Component {
   state = {
     selectedIndex: 0,
-    listItems: [
-      {
-        id: 1,
-        name: 'Amy Farha',
-        avatar_url:
-          'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-        subtitle: 'Vice President',
-      },
-      {
-        id: 2,
-        name: 'Chris Jackson',
-        avatar_url:
-          'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-        subtitle: 'Vice Chairman',
-      },
-    ],
+    listItems: DatList,
     deletedList: [],
+    selectedList: false,
   };
   updateIndex = (selectedIndex) => {
     if (selectedIndex === 2) {
       this.setState({selectedIndex, listItems: []});
     } else if (selectedIndex === 1) {
       this.setState({selectedIndex});
+      this.selectAll();
     } else {
       this.setState({selectedIndex});
     }
   };
 
+  selectAll = () => {
+    let result = this.state.listItems.map((item) =>
+      !this.state.selectedList ? item.id : [],
+    );
+
+    this.setState({
+      deletedList: result,
+      selectedList: !this.state.selectedList,
+    });
+  };
+
+  delItem = (id) => {
+    let helperArray = this.state.listItems;
+    helperArray = helperArray.filter((item) => item.id !== id);
+    // helperArray.splice(id, 1);
+    this.setState({listItems: helperArray});
+  };
+
   renderItem = ({item}) => {
     return (
       <ListItem bottomDivider>
-        <CheckBox />
+        <CheckBox checked={this.state.deletedList.includes(item.id)} />
         <Avatar rounded source={{uri: item.avatar_url}} />
         <ListItem.Content>
           <ListItem.Title>{item.name}</ListItem.Title>
           <ListItem.Subtitle>{item.subtitle}</ListItem.Subtitle>
         </ListItem.Content>
-        <FontIcon name="trash-o" color="red" size={25} />
+        <TouchableOpacity
+          onPress={() => {
+            this.delItem(item.id);
+          }}>
+          <FontIcon name="trash-o" color="red" size={25} />
+        </TouchableOpacity>
         <Icon name="chevron-right" color="#eee" size={30} />
       </ListItem>
     );
   };
 
   render() {
-    const buttons = ['Delete Selected', 'Select All', 'DeleteAll'];
+    const buttons = [
+      'Delete Selected',
+      this.state.selectedList ? 'Unselect All' : 'Select All',
+      'DeleteAll',
+    ];
     const {selectedIndex} = this.state;
     return (
       <SafeAreaView style={styles.container}>
---
+++ Components/DataList/DataList.js
@@ -0,0 +1,123 @@
+const listData = [
+  {
+    id: 1,
+    name: 'Amy Farha',
+    avatar_url:
+      'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'Vice President',
+  },
+  {
+    id: 2,
+    name: 'Chris Jackson',
+    avatar_url:
+      'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+    subtitle: 'Vice Chairman',
+  },
+  {
+    id: 3,
+    name: 'Farha Amy',
+    avatar_url:
+      'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'Manager',
+  },
+  {
+    id: 4,
+    name: 'Sally John',
+    avatar_url:
+      'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'Team Lead',
+  },
+  {
+    id: 5,
+    name: 'Don Boss',
+    avatar_url:
+      'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'Team supervisor',
+  },
+  {
+    id: 6,
+    name: 'John Honnay',
+    avatar_url:
+      'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'Team manager',
+  },
+  {
+    id: 7,
+    name: 'Amy watson',
+    avatar_url:
+      'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'Team Lead',
+  },
+  {
+    id: 8,
+    name: 'Shakira',
+    avatar_url:
+      'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'Quality analyst',
+  },
+  {
+    id: 9,
+    name: 'Ola',
+    avatar_url:
+      'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'Tester',
+  },
+  {
+    id: 10,
+    name: 'Stephan',
+    avatar_url:
+      'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'senior programmer',
+  },
+  {
+    id: 11,
+    name: 'Jackson',
+    avatar_url:
+      'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+    subtitle: 'senior programmer',
+  },
+  {
+    id: 12,
+    name: 'Allan Jackson',
+    avatar_url:
+      'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+    subtitle: 'mid programmer',
+  },
+  {
+    id: 13,
+    name: 'Jack sta',
+    avatar_url:
+      'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+    subtitle: 'mid programmer',
+  },
+  {
+    id: 14,
+    name: 'Briton',
+    avatar_url:
+      'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+    subtitle: 'Regular programmer',
+  },
+  {
+    id: 15,
+    name: 'Antony',
+    avatar_url:
+      'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+    subtitle: 'Regular programmer',
+  },
+  {
+    id: 16,
+    name: 'Maagda',
+    avatar_url:
+      'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+    subtitle: 'Junior programmer',
+  },
+  {
+    id: 17,
+    name: 'Karolina',
+    avatar_url:
+      'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+    subtitle: 'Junior programmer',
+  },
+];
+
+export default listData;
